935c6c7260a7b691d319f940d40ce1affd41bab2
Joshua Kattapuram <70146035+joshuakatt@users.noreply.github.com>
2023-07-09 16:38:03 -0230

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_6883Zi6x6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2896p265dc4napsl5rcju2c5ii', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
