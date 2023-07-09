42e0b157c75989bc6bba1d62a12c0522ab8f13c2
Joshua Kattapuram <70146035+joshuakatt@users.noreply.github.com>
2023-07-09 18:47:57 -0230

    api set up

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://43z0kpgel5.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
